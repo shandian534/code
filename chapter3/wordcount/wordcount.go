b8a52c90c98276d2fe66ce16f862518eb824bfcf
liupg <985346375@sina.com>
2025-03-08 00:12:04 +0800

    添加注释

--- chapter3/wordcount/wordcount.go
+++ chapter3/wordcount/wordcount.go
@@ -10,15 +10,17 @@ import (
 // main is the entry point for the application.
 func main() {
 	//filename := os.Args[1]
-
+	//文件读取操作
 	contents, err := os.ReadFile("/Users/shandian/GolandProjects/code/chapter3/wordcount/gowords.txt")
+	//错误处理
 	if err != nil {
 		fmt.Println("There was an error opening the file:", err)
 		return
 	}
-
+	//数据类型转换
 	text := string(contents)
-
+	//调用函数
 	count := words.CountWords(text)
+	//输出
 	fmt.Printf("There are %d words in your text. \n", count)
 }
